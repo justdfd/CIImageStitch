0a3b2dfe79eef314f414ce0ea52158a7204a61e5
dfd <dfd@justdfd.com>
2018-08-30 16:49:05 -0400

    Initial Commit

---
+++ CIImageStitch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CIImageStitch
+//
+//  Created by Dave Dombrowski on 8/30/18.
+//  Copyright © 2018 justDFD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
